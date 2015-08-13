3eb479f15b94fde9c56079eb4b9ef3dc3030c4ab
Tony <tonylefler@gmail.com>
2015-08-13 11:37:44 -0600

    change order of pics

--- dev/properties/properties.js
+++ dev/properties/properties.js
@@ -9,10 +9,10 @@ greerApp.config(function($stateProvider) {
 
 var propertiesCtrl = function($scope) {
     $scope.images = [
-        '/img/properties-fade/IMG_2498.jpg',
+        '/img/properties-fade/3.5.jpg',
         '/img/properties-fade/2.jpg',
         '/img/properties-fade/IMG_2752.jpg',
-        '/img/properties-fade/3.5.jpg',
+        '/img/properties-fade/IMG_2498.jpg',
         '/img/properties-fade/IMG_2740.jpg',
         '/img/properties-fade/5.jpg',
         '/img/properties-fade/6.jpg',
