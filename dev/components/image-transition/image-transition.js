36a2b0e46d4ebc345239d157deed8d40b34d1bc3
Tony Lefler <tonylefler@gmail.com>
2015-08-19 20:52:05 -0700

    added controls to the image transition thingy

--- dev/components/image-transition/image-transition.js
+++ dev/components/image-transition/image-transition.js
@@ -6,92 +6,118 @@ greerApp.directive('imageTransition', function($interval, $compile, $rootScope) 
         });
 
         var self = this;
-        var overlayImage = angular.element('<div class="page-image-overlay"></div>');
+        var imageElements = [];
+        var imageTemplate = angular.element('<div class="image-slide"></div>')
         var buttonNavLeft = angular.element('<div class="property-btn property-btn-left"><i class="gi gi-keyboard_arrow_left"></i></div>')
         var buttonNavRight = angular.element('<div class="property-btn property-btn-right"><i class="gi gi-keyboard_arrow_right"></i></div>')
         var darkenImage = angular.element('<div class="darken-images"></div>');
         var currentIndex = 0;
-        var intervalCount = 0;
         this.interval = '';
         this.images = $scope.images;
 
+
         this.init = function() {
-            $compile(overlayImage)($scope);
+            angular.forEach($scope.images, function(imageUrl, key) {
+                var newImage = imageTemplate.clone();
+                newImage[0].style.backgroundImage = "url('"+imageUrl+"')";
+                if (currentIndex==key) {
+                    newImage[0].style.opacity = 1;
+                }
+                imageElements[key] = {
+                    imageUrl: imageUrl,
+                    element: $compile(newImage)($scope)
+                };
+                $element.append(newImage);
+            });
+
             $compile(buttonNavLeft)($scope);
             $compile(buttonNavRight)($scope);
-            $element.append(overlayImage);
             $element.append(darkenImage);
-            //$element.append(buttonNavRight);
-            //$element.append(buttonNavLeft);
-            overlayImage[0].style.opacity = 0;
+            $element.append(buttonNavRight);
+            $element.append(buttonNavLeft);
 
-            $element[0].style.backgroundImage = "url('"+self.images[currentIndex]+"')";
-            overlayImage[0].style.backgroundImage = "url('"+self.images[getNextIndex(currentIndex)]+"')";
-            currentIndex = getNextIndex(currentIndex);
-
-            // set up images
+            // start interval
             this.startInterval();
         };
 
         this.startInterval = function() {
             self.interval = $interval(function() {
                 self.nextImage();
-                intervalCount++;
             }, 6000);
         };
 
         this.nextImage = function() {
-            if (overlayImage.hasClass('isActive')) {
-                overlayImage.removeClass('isActive');
-                Velocity(overlayImage, {
-                    opacity: 0
-                }, {
-                    duration: 1000,
-                    complete: function() {
-                        loadNextImage()
-                    }
-                })
-            } else {
-                overlayImage.addClass('isActive');
-                Velocity(overlayImage, {
-                    opacity: 1
-                }, {
-                    duration: 1000,
-                    complete: function() {
-                        loadNextImage()
-                    }
-                })
-            }
+
+            Velocity(imageElements[currentIndex].element, 'stop');
+
+            // remove current element
+            Velocity(imageElements[currentIndex].element, {
+                opacity: 0
+            }, {
+                duration: 1000
+            });
+
+            // update index
+            currentIndex = getNextIndex(currentIndex);
+
+            Velocity(imageElements[currentIndex].element, 'stop');
+
+            // show new current element
+            Velocity(imageElements[currentIndex].element, {
+                opacity: 1
+            }, {
+                duration: 1000
+            });
         };
 
-        //$element.on('mouseover', function() {
-        //    self.stopInterval();
-        //});
-        //
-        //$element.on('mouseleave', function() {
-        //    self.startInterval();
-        //});
+        this.previousImage = function() {
+            Velocity(imageElements[currentIndex].element, 'stop');
+
+            // remove current element
+            Velocity(imageElements[currentIndex].element, {
+                opacity: 0
+            }, {
+                duration: 1000
+            });
+
+            // update index
+            currentIndex = getPreviousIndex(currentIndex);
+
+            Velocity(imageElements[currentIndex].element, 'stop');
+
+            // show new current element
+            Velocity(imageElements[currentIndex].element, {
+                opacity: 1
+            }, {
+                duration: 1000
+            });
+        };
+
+        $element.on('mouseover', function() {
+            self.stopInterval();
+        });
+
+        $element.on('mouseleave', function() {
+            self.startInterval();
+        });
 
         buttonNavLeft.on('click', function() {
-
+            self.previousImage();
         });
 
         buttonNavRight.on('click', function() {
             self.nextImage();
-            intervalCount++;
         });
 
         var loadNextImage = function() {
-            if (intervalCount%2) {
-                $element[0].style.backgroundImage = "url('"+self.images[getNextIndex(currentIndex)]+"')";
-            } else {
-                overlayImage[0].style.backgroundImage = "url('"+self.images[getNextIndex(currentIndex)]+"')";
-            }
-            currentIndex = getNextIndex(currentIndex);
         };
 
-        var getNextIndex = function(currentIndex) {
-            return currentIndex+1>self.images.length-1?0:currentIndex+1;
+        var getNextIndex = function(index) {
+            return index+1>self.images.length-1?0:index+1;
+        };
+
+        var getPreviousIndex = function(index) {
+            return index==0?self.images.length-1:index-1;
         };
 
         this.stopInterval = function() {
